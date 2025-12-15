fe1ebedbddcc1598f73ab8b003da9cf0fd881544
Taha Farshbaf <tahafarshbaf@outlook.com>
2025-12-15 10:49:36 +0330

    updated

--- AMOS GILAT/AMOS_GILAT_CH6_P15.m
+++ AMOS GILAT/AMOS_GILAT_CH6_P15.m
@@ -1,15 +1,23 @@
 x = [-3.5 -5 6.2 11 0 8.1 -9 0 3 -1 3 2.5];
 
-P = [];   
-N = [];   
+P = zeros(1, length(x));
+N = zeros(1, length(x));
+
+ip = 0;
+in = 0;
 
 for i = 1:length(x)
     if x(i) > 0
-        P = [P x(i)];
+        ip = ip + 1;
+        P(ip) = x(i);
     elseif x(i) < 0
-        N = [N x(i)];
+        in = in + 1;
+        N(in) = x(i);
     end
 end
+
+P = P(1:ip);
+N = N(1:in);
 
 disp('P = '), disp(P)
 disp('N = '), disp(N)
